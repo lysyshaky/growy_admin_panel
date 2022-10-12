2f5e8ec52156949e7155931dcac11388a70fd650
Woloyor <yuralysyshak@gmail.com>
2022-10-12 14:17:18 +0300

    admin dashboard

---
+++ lib/responsive.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  // final Widget? tablet;
+  final Widget desktop;
+
+  const Responsive({Key? key, required this.mobile, required this.desktop})
+      : super(key: key);
+
+  static bool isMoble(BuildContext context) =>
+      MediaQuery.of(context).size.width < 850;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1100;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    if (_size.width >= 1100) {
+      return desktop;
+    } else {
+      return mobile;
+    }
+  }
+}
---
+++ lib/services/utils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:growy_admin_panel/provider/dart_theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class Utils {
+  BuildContext context;
+  Utils(this.context);
+  bool get getTheme => Provider.of<DarkThemeProvider>(context).getDarkTheme;
+  get appBarcolor => getTheme ? Colors.green : Colors.white;
+  get color => getTheme ? Colors.white : Colors.black;
+  Size get getScreenSize => MediaQuery.of(context).size;
+}
